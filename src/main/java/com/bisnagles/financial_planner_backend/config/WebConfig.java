75bfd66302144903f92a49c5e3a2872b9c4763e9
Gabriel Moreira <youremail@example.com>
2024-10-06 19:31:25 -0400

    Allowing CORS

--- src/main/java/com/bisnagles/financial_planner_backend/config/WebConfig.java
+++ src/main/java/com/bisnagles/financial_planner_backend/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")  // Replace with your Next.js URL
+                .allowedOrigins("*")  // Replace with your Next.js URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
